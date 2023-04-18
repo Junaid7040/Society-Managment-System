2d82c1b437d48913a15bb127ed0d669a10ea6060
Junaid7040 <88629240+Junaid7040@users.noreply.github.com>
2023-04-18 14:08:53 +0500

    Delete index.controller.js

--- index.controller.js
+++
@@ -1,9 +0,0 @@
-exports.Home = function (req, res) {
-  res.send("Home page");
-};
-exports.About = function (req, res) {
-  res.send("About Us Page");
-};
-exports.Contact = function (req, res) {
-  res.send("Contact Us Page");
-};
